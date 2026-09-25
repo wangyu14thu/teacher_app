0d5f23feafdd58d9285a584301508e6ab7513e83
yuwang <yuwang@deeproute.ai>
2026-09-25 13:16:29 +0800

    style: refresh ledger with gold glassmorphism theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fangcun-v1";
+const CACHE = "fangcun-v2-gold";
 const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
