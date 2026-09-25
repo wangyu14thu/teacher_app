7045a046d338a7cc514a5d9050031729cd8468a1
yuwang <yuwang@deeproute.ai>
2026-09-25 12:55:48 +0800

    delete all

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "fangcun-v1";
+const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
