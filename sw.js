5e1344f0e4f19a4abcad5a881455a8ba983a876f
yuwang <yuwang@deeproute.ai>
2026-09-25 13:59:53 +0800

    deploy: prepare asset ledger for GitHub Pages installation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "fangcun-v4-banklogos";
+const CACHE = "fangcun-v5-pages";
 const BANK_LOGOS = ["abc.svg", "bob.svg", "boc.svg", "bocom.svg", "bosc.svg", "ccb.svg", "ceb.svg", "cgb.svg", "cib.svg", "citic.svg", "cmb.svg", "cmbc.svg", "hxb.svg", "icbc.svg", "nbcb.svg", "pab.svg", "psbc.svg", "spdb.svg"].map(file => `./assets/banks/${file}`);
-const FILES = ["./", "./index.html?v=banklogos-4", "./styles.css?v=banklogos-4", "./app.js?v=banklogos-4", "./manifest.webmanifest", "./icon.svg", ...BANK_LOGOS];
+const FILES = ["./", "./index.html", "./styles.css?v=banklogos-4", "./app.js?v=banklogos-4", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", ...BANK_LOGOS];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
