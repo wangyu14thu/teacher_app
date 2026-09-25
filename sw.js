57980206f60b722cd7c8be779e7d023832b1a7e3
yuwang <yuwang@deeproute.ai>
2026-09-25 13:26:06 +0800

    fix: bust stale service worker assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "fangcun-v2-gold";
-const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const FILES = ["./", "./index.html?v=gold-3", "./styles.css?v=gold-3", "./app.js?v=gold-3", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
